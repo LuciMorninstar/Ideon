e287c225f8e84ec44b90bc6a9a6b9dd3bbc6884e
Stars_Winner <stars.winner1121@gmail.com>
2026-04-06 23:11:57 +0545

    Implemented updatedPost controller

--- backend/src/models/post.model.js
+++ backend/src/models/post.model.js
@@ -56,7 +56,12 @@ const postSchema = new mongoose.Schema({
             type:mongoose.Schema.Types.ObjectId,
             ref:"User"
         }
-    ]
+    ],
+
+    isEdited:{
+        type:Boolean,
+        default:false
+    }
 
 
 
