f105101b153ddcb7b77cc9f050cab97599969207
Stars_Winner <stars.winner1121@gmail.com>
2026-04-11 23:16:33 +0545

    Added sendMessage,getMyMessages,getConversationMessages features in message.controller.js

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -4,7 +4,7 @@ import { deleteFromCloudinary, uploadOnCloudinary } from "../utils/cloudinary.js
 
 export const sendMessage = async(req,res,next)=>{
     const myId = req.user?.id;
-    const {receiverId} = req.body;
+    const {receiverId} = req.params;
     const {text} = req.body;
 
     try {
@@ -68,3 +68,104 @@ export const sendMessage = async(req,res,next)=>{
     }
 
 }
+
+
+
+//To get all the Messages of mine (not of an individual chat)
+export const getMyMessages = async(req,res,next)=>{
+
+    const myId = req.user?.id;
+
+    try {
+        if(!myId){
+            const err = new Error("UnAuthorized Access");
+            err.statusCode = 401;
+            return next(err);
+        }
+
+        const myMessages = await Message.find(
+            {
+                $or:[
+                    {senderId:myId},
+                    {receiverId:myId}
+                ]
+            }
+        ).sort({createdAt:1})
+
+        if(myMessages.length === 0){
+            return res.status(200).json({
+                success:true,
+                message:"No messages found",
+                myMessages:[]
+            })
+        }
+
+        return res.status(200).json({
+            success:true,
+            message:"Successfully found myMessages",
+            myMessages:myMessages
+
+        })
+
+    
+    } catch (error) {
+        console.log(`Error in the getMyMessages Controller : ${error.message}`);
+        next(error);
+        
+    }
+
+
+}
+
+//To get chats related to individual
+
+export const getConversationMessages = async(req,res,next)=>{
+
+    const myId = req.user?.id;
+    const {friendId} = req.params;
+
+    try {
+
+        if(!myId){
+            const err = new Error("UnAuthorized Access");
+            err.statusCode = 401;
+            return next(err);
+        }
+
+        if(!friendId){
+            const err = new Error("No friendId provided");
+            err.statusCode = 404;
+            return next(err);
+        }
+
+        const messages = await Message.find({
+            $or:[
+                {senderId:myId, receiverId:friendId},
+                {senderId:friendId, receiverId:myId}
+            ]
+        }).sort({createdAt:1})
+
+        if(messages.length === 0 ){
+            return res.status(200).json({
+                success:true,
+                message: "No messages found!",
+                messages:[]
+            })
+        }
+
+        return res.status(200).json({
+            success:true,
+            message:"Messages found Successfully",
+            messages:messages
+        })
+        
+
+    } catch (error) {
+        console.log( `Error in the getConversationMessages Controller : ${error.message}`);
+        next(error);
+        
+    }
+
+
+}
+
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,12 @@
 import express from "express"
 import { protectRoute } from "../middlewares/auth.middleware.js";
-import { sendMessage } from "../controllers/message.controller.js";
+import { getConversationMessages, getMyMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 
+router.get("/", protectRoute, getMyMessages);
+router.get("/:friendId", protectRoute, getConversationMessages);
 router.post("/:receiverId", protectRoute,sendMessage);
 
 
